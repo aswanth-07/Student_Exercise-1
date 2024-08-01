c369ddfcb94803ff65af84b50f2ef0a54c7ef331
A Aswanth Raj <a.aswanth0707@gmail.com>
2024-08-01 23:27:15 +0530

    Update ex-3.py

--- ex-3.py
+++ ex-3.py
@@ -1,3 +1,10 @@
+#establishing the list numbers
 numbers = [1,2,3,4,5,6,7,8,9,10]
+#slicing the list
+print("first five elements of the list is :",numbers[0,5])
+#using for loop to print the elements of the list
+for i in numbers :
+  print(i)
+#List comprehension
 squares = [x**2 for x in numbers]
 print(squares)
